69c94736a4554e5b6cf61358a33104850c325236
Potato <137735702+rthjr@users.noreply.github.com>
2024-06-26 15:40:58 +0700

    d

--- app/components/footerpage/FooterPage.tsx
+++ app/components/footerpage/FooterPage.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
 
     return (
         <PageWrapper>
-            <div className='bg-stone-600 w-full h-auto p-5 md:p-10'>
+            <div className='grid gap-10 md:gap-14 bg-stone-600 w-full h-auto p-5 md:p-10'>
                 <div className='grid md:grid-cols-2 grid-cols-1 font-bold text-white'>
                     <div className='flex md:justify-center justify-start'>
                         <h4>Email: sovannarithphan9@gmail.com</h4>
@@ -16,15 +16,16 @@ const Footer = () => {
                         <h4>Phone: 092-559-662</h4>
                     </div>
                 </div>
-                <div className='flex md:justify-center md:items-center justify-start font-bold text-white md:mt-10 mt5'>
-                    <p>&copy; {currentYear} Phan Sovannarith. All rights reserved.</p>
-                </div>
                 <div className="flex justify-center items-center mt-24">
                     <a href="#home">
                         <svg className="animate-bounce w-12 h-12 text-blue-500" fill="currentColor" viewBox="0 0 24 24">
                             <FaArrowUp className='text-white' />
                         </svg>
                     </a>
+                </div>
+                
+                <div className='flex md:justify-center md:items-center justify-center font-bold text-white'>
+                    <p>&copy; {currentYear} Phan Sovannarith. All rights reserved.</p>
                 </div>
             </div>
         </PageWrapper>
